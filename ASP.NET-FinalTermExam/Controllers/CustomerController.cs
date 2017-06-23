a5dd6a16466209454a53af0147fde930c0bde909
user <user@MA324-3>
2017-06-23 16:12:10 +0800

    ASP.NET-FinalTermExam

---
+++ ASP.NET-FinalTermExam/Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_FinalTermExam.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerService.CustomerService cs = new CustomerService.CustomerService();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            ViewBag.ContactTitle = cs.GetContactTitle();
+            return View();
+        }
+        public JsonResult SearchResult(CustomerModel.Customer customer)
+        {
+            var result = cs.GetCustomer(customer);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult InsertCustomer()
+        {
+            ViewBag.ContactTitle = cs.GetContactTitle();
+            return View();
+        }
+        public ActionResult InsertCustomerAction(CustomerModel.Customer customer) {
+            ViewBag.CustomerID=cs.InsertCustomer(customer);
+            return Redirect("Index");
+        }
+    }
+}
---
+++ CustomerService/CustomerService.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+namespace CustomerService
+{
+    public class CustomerService
+    {
+        protected string GetDBconnectionstring()
+        {
+            return ConfigurationManager.ConnectionStrings["DBconn"].ConnectionString;
+        }
+        private List<CustomerModel.Customer> MapCustomerDataToList(DataTable dt)
+        {
+            List<CustomerModel.Customer> result = new List<CustomerModel.Customer>();
+            foreach (DataRow row in dt.Rows)
+            {
+                result.Add(new CustomerModel.Customer()
+                {
+                    CustomerID = Convert.ToString(row["CustomerID"]),
+                    CompanyName = (string)row["CompanyName"],
+                    ContactName = (string)row["ContactName"],
+                    ContactTitle = (string)row["Codeval"]
+                });
+            }
+            return result;
+        }
+        public List<CustomerModel.Customer> GetCustomer(CustomerModel.Customer customer)
+        {
+            DataTable dt = new DataTable();
+            string SQL = @"Select CustomerId,CompanyName,ContactName,Codeval from Sales.Customers a join dbo.CodeTable b on a.ContactTitle=b.CodeId where CustomerID like @CustomerID and
+                            CompanyName like @CompanyName and ContactName like @ContactName and ContactTitle like @ContactTitle and CodeType='TITLE'";
+            using (SqlConnection conn = new SqlConnection(this.GetDBconnectionstring()))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(SQL, conn);
+                    cmd.Parameters.Add(new SqlParameter("@CustomerID", customer.CustomerID == null? "%%" : "%" + Convert.ToString(customer.CustomerID) + "%"));
+                    cmd.Parameters.Add(new SqlParameter("@CompanyName",  customer.CompanyName== null ? "%%" : "%" + Convert.ToString(customer.CompanyName) + "%"));
+                    cmd.Parameters.Add(new SqlParameter("@ContactName", customer.ContactName == null ? "%%" : Convert.ToString(customer.ContactName)));
+                    cmd.Parameters.Add(new SqlParameter("@ContactTitle", customer.ContactTitle == null ? "%%" : Convert.ToString(customer.ContactTitle)));
+                    SqlDataAdapter ad = new SqlDataAdapter(cmd);
+                    ad.Fill(dt);
+                    conn.Close();
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.WriteLine(e);
+                }
+            }
+            return this.MapCustomerDataToList(dt);
+        }
+        public List<SelectListItem> GetContactTitle()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            string SQL = "Select CodeId,CodeVal from CodeTable where CodeType='Title'";
+            using (SqlConnection conn = new SqlConnection(this.GetDBconnectionstring()))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(SQL, conn);
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    while (rd.Read())
+                    {
+                        list.Add(new SelectListItem { Text = rd[1].ToString(), Value = rd[0].ToString() });
+                    }
+                    conn.Close();
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.WriteLine(e);
+                }
+                return list;
+            }
+        }
+        public int InsertCustomer(CustomerModel.Customer customer)
+        {
+            string SQL = @"Insert into Sales.Customers values(
+                         @CompanyName,@ContactName,@ContactTitle,@CreationDate,@Address,@City,
+                         @Region,@PostalCode,@Country,@Phone,@Fax)
+                         Select Cast(SCOPE_IDENTITY() as int)";
+            int id = 0;
+            using (SqlConnection conn = new SqlConnection(this.GetDBconnectionstring()))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(SQL, conn);
+                    cmd.Parameters.Add(new SqlParameter("@CompanyName", customer.CompanyName));
+                    cmd.Parameters.Add(new SqlParameter("@ContactName", customer.ContactName));
+                    cmd.Parameters.Add(new SqlParameter("@ContactTitle", customer.ContactTitle));
+                    cmd.Parameters.Add(new SqlParameter("@CreationDate", string.Format("{0:yyyy/MM/dd}", customer.CreationDate)));
+                    cmd.Parameters.Add(new SqlParameter("@Address", customer.Address));
+                    cmd.Parameters.Add(new SqlParameter("@City", customer.City));
+                    cmd.Parameters.Add(new SqlParameter("@Region", customer.Region));
+                    cmd.Parameters.Add(new SqlParameter("@PostalCode", customer.PostalCode));
+                    cmd.Parameters.Add(new SqlParameter("@Country", customer.Country));
+                    cmd.Parameters.Add(new SqlParameter("@Phone", customer.Phone));
+                    cmd.Parameters.Add(new SqlParameter("@Fax", customer.Fax));
+                    id = (int)cmd.ExecuteScalar();
+                    conn.Close();
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.WriteLine(e);
+                }
+            }
+            return id;
+        }
+    }
+}
